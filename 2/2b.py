15fe2f5282e24a8e2dd5387dcc0de4721ff2925d
Bo Chen <bo@liftoff.io>
2025-12-02 08:58:39 -0800

    2

---
+++ 2/2b.py
@@ -0,0 +1,47 @@
+import math
+import os
+import re
+import sys
+import copy
+from functools import *
+from pathlib import Path
+
+source_path = Path(__file__).resolve()
+source_dir = source_path.parent
+
+def pm(m):
+    for r in m:
+        s = ""
+        for c in r:
+            print(c, end="")
+        print("")
+
+def cpm(m):
+    return [r[:] for r in m]
+
+def stoia(pstr, delim = ","):
+    return list(map(lambda x: int(x), pstr.strip().split(delim)))
+
+def iatos(p):
+    return ",".join(map(lambda x: str(x), p))
+
+ls = []
+with open(f'{source_dir}/in.txt') as fp:
+    for line in fp:
+        lstripped = line.strip()
+
+        if lstripped != "":
+            ls.append(lstripped)
+
+t = 0
+for r in ls[0].split(","):
+    [s, e] = r.split("-")
+    for n in range(int(s),int(e) + 1):
+        sn = str(n)
+        if re.match(r"^(.+)\1+$",sn):
+            t += n
+
+print(t)
+
+
+
