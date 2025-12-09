33521e0252be6c261478c8dfd25a7b4acf5424a1
Bo Chen <bo@liftoff.io>
2025-12-09 00:59:40 -0500

    better naming

--- 9/9b.py
+++ 9/9b.py
@@ -33,23 +33,23 @@ with open(f'{source_dir}/in.txt') as fp:
         if lstripped != "":
             ls.append(lstripped)
 
-ps = []
+points = []
 
-vs = []
-hs = []
+vsegs = []
+hsegs = []
 def irs(p, lastp):
-    global hs
-    global vs
+    global hsegs
+    global vsegs
     if p[0] == lastp[0]:
         if p[1] >= lastp[1]:
-            vs.append([[p[0],lastp[1]],[p[0],p[1]]])       
+            vsegs.append([[p[0],lastp[1]],[p[0],p[1]]])       
         else:
-            vs.append([[p[0],p[1]], [p[0],lastp[1]]])
+            vsegs.append([[p[0],p[1]], [p[0],lastp[1]]])
     elif p[1] == lastp[1]:
         if p[0] >= lastp[0]:
-            hs.append([[lastp[0],p[1]], [p[0], p[1]]])       
+            hsegs.append([[lastp[0],p[1]], [p[0], p[1]]])       
         else:
-            hs.append([[p[0],p[1]], [lastp[0],p[1]]])
+            hsegs.append([[p[0],p[1]], [lastp[0],p[1]]])
     else:
         print("bad")
         exit(0)
@@ -57,31 +57,31 @@ def irs(p, lastp):
 lastp = None
 for l in ls:
     p = stoia(l)
-    ps.append(p)
+    points.append(p)
     if lastp is not None:
         irs(p, lastp)
     lastp = p
 
-irs(lastp, ps[0])
+irs(lastp, points[0])
 
-def checki(s1):
-    if s1[0][0] == s1[1][0]:
-        if s1[0][1] > s1[1][1]:
-            s1 = [s1[1], s1[0]]
-        for rp in hs:
-            if s1[0][1] < rp[0][1] and \
-                    rp[0][1] < s1[1][1] and \
-                    rp[0][0] < s1[0][0] and \
-                    s1[0][0] < rp[1][0]:
+def checkseginter(seg):
+    if seg[0][0] == seg[1][0]:
+        if seg[0][1] > seg[1][1]:
+            seg = [seg[1], seg[0]]
+        for hseg in hsegs:
+            if seg[0][1] < hseg[0][1] and \
+                    hseg[0][1] < seg[1][1] and \
+                    hseg[0][0] < seg[0][0] and \
+                    seg[0][0] < hseg[1][0]:
                 return True
-    elif s1[0][1] == s1[1][1]:
-        if s1[0][0] > s1[1][0]:
-            s1 = [s1[1], s1[0]]
-        for rp in vs:
-            if rp[0][1] < s1[0][1] and \
-                    s1[0][1] < rp[1][1] and \
-                    s1[0][0] < rp[0][0] and \
-                    rp[0][0] < s1[1][0]:
+    elif seg[0][1] == seg[1][1]:
+        if seg[0][0] > seg[1][0]:
+            seg = [seg[1], seg[0]]
+        for vseg in vsegs:
+            if vseg[0][1] < seg[0][1] and \
+                    seg[0][1] < vseg[1][1] and \
+                    seg[0][0] < vseg[0][0] and \
+                    vseg[0][0] < seg[1][0]:
                 return True
     else:
         print("bad2")
@@ -90,23 +90,23 @@ def checki(s1):
     return False
         
 
-def checkborders(p1, p2):
+def checkbordersinter(p1, p2):
     xs = sorted([p1[0],p2[0]])
     ys = sorted([p1[1],p2[1]])
 
-    if checki([[xs[0],ys[0] + 0.1 ], [xs[1], ys[0] + 0.1]]) or\
-            checki([[xs[1] - 0.1,ys[0]], [xs[1] - 0.1, ys[1]]]) or\
-            checki([[xs[1],ys[1] - 0.1], [xs[0], ys[1] - 0.1]]) or\
-            checki([[xs[0] + 0.1,ys[1]], [xs[0] + 0.1, ys[0]]]):
+    if checkseginter([[xs[0],ys[0] + 0.1 ], [xs[1], ys[0] + 0.1]]) or\
+            checkseginter([[xs[1] - 0.1,ys[0]], [xs[1] - 0.1, ys[1]]]) or\
+            checkseginter([[xs[1],ys[1] - 0.1], [xs[0], ys[1] - 0.1]]) or\
+            checkseginter([[xs[0] + 0.1,ys[1]], [xs[0] + 0.1, ys[0]]]):
         return True
 
 ms = 0
 
-for p1 in ps:
-    for p2 in ps:
+for p1 in points:
+    for p2 in points:
         if p1 == p2:
             continue
-        if not checkborders(p1, p2):
+        if not checkbordersinter(p1, p2):
             s = (abs(p2[0] - p1[0]) + 1) * (abs(p2[1] - p1[1]) + 1)
             if s > ms:
                 ms = s
